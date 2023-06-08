ea0406b62dfc070d1cce2591451993239f3c2098
Pyriel <61353556+PRoyale@users.noreply.github.com>
2023-06-08 08:50:00 +0100

    Update player.js
    adding alert boxes for debugging

--- royale-client/src/main/webapp/js/app/game/object/player.js
+++ royale-client/src/main/webapp/js/app/game/object/player.js
@@ -1182,6 +1182,8 @@ PlayerObject.prototype.warpRelative = function(paramData) {
   var coords = paramData.data.split(",");
   this.pos.x += parseInt(coords[1]);
   this.pos.y += parseInt(coords[2]);
+  alert(coords[1]);
+  alert(coords[2]);
 };
 
 /* ent/ext = null, up, down, left, right [0,1,2,3,4] */
